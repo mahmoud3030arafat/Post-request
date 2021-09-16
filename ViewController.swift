88fe64bcc7763b443939d0dd6b3df162e88dfa9c
Mahmoud Arafat <81111174+mahmoud3030arafat@users.noreply.github.com>
2021-09-16 05:31:45 -0700

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -13,8 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
 
-        // create an instance of the Post struct with your own values
-      //  let post = Post(userId: 200, title: "mahmoud", body: "udacious")
+       
+  
 
         // create a URLRequest by passing in the URL
         var request = URLRequest(url: URL(string: "https://jsonplaceholder.typicode.com/posts")!)
@@ -31,8 +31,8 @@ class ViewController: UIViewController {
          }
       
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-       // request.addValue("ar", forHTTPHeaderField: "lang")
-        // HACK: this line allows the workspace or an Xcode playground to execute the requeslangt, but is not needed in a real app
+    
+     
        
         // task for making the request
         let task = URLSession.shared.dataTask(with: request) {data, response, error in
